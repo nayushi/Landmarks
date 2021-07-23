30c6df26548244a3d69e7f3adeb0e06fcc0e6037
Mariana Brasil <nayushi@qq.com>
2021-07-22 21:28:05 -0300

    Building lists and navigation

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,34 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Mariana Brasil on 22/07/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) {
+                landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+    
+}
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+                   LandmarkList()
+                       .previewDevice(PreviewDevice(rawValue: deviceName))
+                       .previewDisplayName(deviceName)
+               }
+
+    }
+}
+
